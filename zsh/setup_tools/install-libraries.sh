531adc1a5434d76141a3349a947819dc8b25212c
Kazuya Iriguchi <baskin.robbins1945@gmail.com>
2020-01-20 22:00:19 +0900

    fix: yes/no branch

--- zsh/setup_tools/install-libraries.sh
+++ zsh/setup_tools/install-libraries.sh
@@ -2,6 +2,6 @@
 library_list=(finger xsel)
 printf '\033[33m%s\033[m\n' "install ${library_list[@]} from now."
 read -p "Are you ok? (y/N): " yn
-case "$yn" in [yN]*) ;; *) echo "abort." ; exit ;; esac
+case "$yn" in [yY]*) ;; *) echo "abort." ; exit ;; esac
 printf '\033[33m%s\033[m\n' "install..."
 sudo apt install -y ${library_list[@]}
