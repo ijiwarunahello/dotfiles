9247eda5435d84bfbcf40c4455fad3ccc949190b
Kazuya Iriguchi <baskin.robbins1945@gmail.com>
2020-06-24 19:17:01 +0900

    fix: interactively config  user.name/email

--- zsh/setup_tools/setting-git-config.sh
+++ zsh/setup_tools/setting-git-config.sh
@@ -1,32 +1,20 @@
 #!/bin/bash
 printf '\033[33m%s\033[m\n' "setting git config..."
 # Get arg
-USERNAME=$1
-EMAIL=$2
 
 # Get current config
-username=`git config --global user.name`
-email=`git config --global user.email`
+USERNAME=`git config --global user.name`
+EMAIL=`git config --global user.email`
 
-function usage() {
-    NAME=`basename $0`
-    echo $NAME USERNAME EMAIL
-}
-
-# already setting username or email
-if [ "$username" == '' ]; then
-    if [ "$USERNAME" == '' ]; then
-        usage
-        exit 1
-    fi
-    git config --global user.name "$USERNAME"
+printf '\033[33m%s\033[m\n' "username"
+if [ "$USERNAME" == '' ]; then
+    read -p "username: " username
+    git config --global user.name "$username"
 fi
-if [ "$email" == '' ]; then
-    if [ "$EMAIL" == '' ]; then
-        usage
-        exit 1
-    fi
-    git config --global user.email $EMAIL
+printf '\033[33m%s\033[m\n' "email"
+if [ "$EMAIL" == '' ]; then
+    read -p "email: " email
+    git config --global user.email "$email"
 fi
 
 git config --global color.ui auto
